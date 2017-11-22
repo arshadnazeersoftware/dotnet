f7986d77ee8df9e164568490f1768c8e8f02818d
arshadnazeersoftware <arshadnazeer@live.com>
2017-11-22 17:27:26 +0530

    C#

--- helloworldapp/Program.cs
+++ helloworldapp/Program.cs
@@ -6,7 +6,46 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello to my World.");
+            if(123 is String)
+            Console.WriteLine(String.Format("Hello to my World.","Hello") as String);
+            else
+            {
+                Console.WriteLine("if not printed");
+            }
+            Mammal z = returnZebra();
+        z.drink();
+         eat(new Zebra());
         }
+        public void eat(Mammal m)
+    {
+        System.Console.WriteLine("Eat");
     }
+
+    public Zebra returnZebra()
+    {
+        return new Zebra();
+    }
+    }
+
+    public class Mammal
+    {
+public virtual void drink()
+{
+System.Console.WriteLine("drink");
+}
+    }
+
+    public class Zebra : Mammal
+    {
+       
+        
+        public override void drink()
+        {
+        System.Console.WriteLine("drink zebra");
+        }
+        
+    }
+
+
+    
 }
